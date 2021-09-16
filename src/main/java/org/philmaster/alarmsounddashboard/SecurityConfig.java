41af10b6031ba59706d2e242f1f7adf0ed64da1f
Philmasteryeah <p.schulz89@gmx.de>
2021-09-17 01:15:32 +0200

    # prepared for vue

--- src/main/java/org/philmaster/alarmsounddashboard/SecurityConfig.java
+++ src/main/java/org/philmaster/alarmsounddashboard/SecurityConfig.java
@@ -1,16 +1,39 @@
 package org.philmaster.alarmsounddashboard;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableJpaRepositories
 //@EnableWebMvc
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.cors();
+		http.csrf();
+
+//		security.cors()
+//				.disable();
+//		security.csrf()
+//				.disable()
+//				.authorizeRequests()
+//				.anyRequest()
+//				.authenticated()
+//				.and()
+//				.formLogin()
+//				.disable(); // <-- this w^ill disable the login route
+
+	}
 //
 ////	// TODO
 ////	@Bean
--- src/main/java/org/philmaster/alarmsounddashboard/controller/KioskControllerRest.java
+++ src/main/java/org/philmaster/alarmsounddashboard/controller/KioskControllerRest.java
@@ -8,12 +8,14 @@ import org.philmaster.alarmsounddashboard.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:8080", maxAge = 3600, allowCredentials = "true")
 @RestController
 @RequestMapping("/api")
 public class KioskControllerRest {
@@ -36,7 +38,7 @@ public class KioskControllerRest {
 	public void delete(Model model) {
 		service.deleteOneRandom();
 	}
-	
+
 	@GetMapping(value = "/deleteAll")
 	public void deleteAll(Model model) {
 		service.deleteAll();
